988e4db1d4cc75b34fd7dd519fbe8d36b0d91d73
tfbrother <tfbrother@qq.com>
2018-11-12 17:48:01 +0800

    fix 整型转字符串bug

--- cli/main.go
+++ cli/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/tfbrother/tfcache"
+	"strconv"
 )
 
 var (
@@ -13,13 +14,14 @@ var (
 
 func main() {
 	for i := 0; i < 20; i++ {
-		cache.Set(string(i), i)
+		// 整型转字符串，不能直接使用string(i)，此时i会被当成ascii来对待
+		cache.Set(strconv.Itoa(i), i)
 	}
 	for i := 0; i < 20; i++ {
-		if value, err = cache.Get(string(i)); err != nil {
+		if value, err = cache.Get(strconv.Itoa(i)); err != nil {
 			fmt.Println(err)
 		} else {
-			fmt.Println("key:", string(i), "value:", value)
+			fmt.Println("key:", strconv.Itoa(i), "value:", value)
 		}
 	}
 }
