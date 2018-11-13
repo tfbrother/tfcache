8ed26a15022a52fe4ad63a70dacea9cd2724853c
tfbrother <tfbrother@qq.com>
2018-11-13 15:10:02 +0800

    提供http接口用于set/get cache

--- cli/main.go
+++ cli/main.go
@@ -5,14 +5,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/tfbrother/tfcache"
+	"net/http"
 	"strconv"
-	"sync"
 )
 
 var (
-	cache *tfcache.Cache = tfcache.NewCache(100) //限制缓存的最大数量
-	err   error
-	value interface{}
+	cache *tfcache.Cache = tfcache.NewCache(1500000) //限制缓存的最大数量
 )
 
 //输出结构体
@@ -29,28 +27,66 @@ func ToString(conf interface{}) string {
 	return out.String()
 }
 
+// 初始化cache
+func init() {
+	for i := 0; i < 2000000; i++ {
+		// 整型转字符串，不能直接使用string(i)，此时i会被当成ascii来对待
+		cache.Set(strconv.Itoa(i), i)
+	}
+}
+
 func main() {
-	var wait sync.WaitGroup
-	wait.Add(2)
 	go func() {
-		for i := 0; i < 200; i++ {
-			// 整型转字符串，不能直接使用string(i)，此时i会被当成ascii来对待
-			cache.Set(strconv.Itoa(i), i)
+		for i := 0; i < 2000000; i++ {
+			cache.Get(strconv.Itoa(i))
 		}
-		wait.Done()
 	}()
 
-	go func() {
-		for i := 0; i < 200; i++ {
-			if value, err = cache.Get(strconv.Itoa(i)); err != nil {
-				fmt.Println(err)
-			} else {
-				fmt.Println("key:", strconv.Itoa(i), "value:", value)
-			}
-		}
-		wait.Done()
-	}()
-	wait.Wait()
+	http.HandleFunc("/stats", handleStats)
+	http.HandleFunc("/setCache", handleSet)
+	http.HandleFunc("/getCache", handleGet)
+
+	http.ListenAndServe("0.0.0.0:7777", nil)
+}
+
+func handleStats(resp http.ResponseWriter, req *http.Request) {
 	stats := cache.Stats()
-	fmt.Println(ToString(&stats))
+
+	resp.Write([]byte(ToString(&stats)))
+}
+
+// 获取缓存
+// curl http://localhost:7777/getCache?key=tfbrother
+func handleGet(resp http.ResponseWriter, req *http.Request) {
+	var (
+		err   error
+		key   string
+		value interface{}
+	)
+
+	key = req.URL.Query().Get("key")
+	if value, err = cache.Get(key); err == nil {
+		//类型断言
+		resp.Write([]byte(value.(string)))
+	} else {
+		resp.Write([]byte(err.Error()))
+	}
+
+}
+
+// 设置缓存
+// curl http://localhost:7777/setCache?key=tfbrother&value=22
+func handleSet(resp http.ResponseWriter, req *http.Request) {
+	var (
+		err        error
+		key, value string
+	)
+
+	key = req.URL.Query().Get("key")
+	value = req.URL.Query().Get("value")
+	if err = cache.Set(key, value); err == nil {
+		resp.Write([]byte("sucess!"))
+	} else {
+		resp.Write([]byte(err.Error()))
+	}
 }
